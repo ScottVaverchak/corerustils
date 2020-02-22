b61fca3f57d38c0612c3f7c4057d3548aabae22c
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-02-21 20:10:15 -0500

    yes has arguments

--- yes/src/main.rs
+++ yes/src/main.rs
@@ -1,5 +1,13 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let output = match args.get(1) {
+        Some(x) => x,
+        None => "y"
+    };
+    
     loop {
-        println!("y");
+        println!("{}", output);
     }
 }
