63f17584812703283514c5405924d7f8d83fa223
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-02-27 19:50:48 -0500

    poorly created cat kind of works sometimes - ship it

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Meow-lo, Purr-ld");
+    let args: Vec<String> = env::args().collect();
+
+    let file = match args.get(1) {
+        Some(arg) => arg,
+        _ => {
+            println!("cat: <file> (no options, no problems)");
+            return
+        }
+    };
+
+    let contents = match fs::read_to_string(file) {
+      Ok(c)    => c,
+      Err(err) => {
+        println!("Seems like we got an error");
+        return
+      }
+    };
+
+    println!("{}", contents);
 }
