29d240b63cd7d227910a2092cc6b402ec48ea099
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-03-04 05:06:48 -0500

    rustfmt on cat and starting head

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -3,22 +3,22 @@ use std::fs;
 use std::io::{self, BufRead};
 use std::process;
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct Arguments {
     count: bool,
     count_non_blank: bool,
     single_spaced: bool,
-    files: Vec<String>
+    files: Vec<String>,
 }
 
-fn parse_aruments(args: Vec::<String>) -> Result<Arguments, String> {
+fn parse_aruments(args: Vec<String>) -> Result<Arguments, String> {
     let mut pargs = Arguments {
-       count: false,
-       count_non_blank: false,
-       single_spaced: false,
-       files: vec![]
+        count: false,
+        count_non_blank: false,
+        single_spaced: false,
+        files: vec![],
     };
-    
+
     for (idx, arg) in args.iter().skip(1).enumerate() {
         if !arg.starts_with('-') {
             pargs.files = args[(idx + 1)..].to_vec();
@@ -29,9 +29,9 @@ fn parse_aruments(args: Vec::<String>) -> Result<Arguments, String> {
             "-n" => pargs.count = true,
             "-b" => pargs.count_non_blank = true,
             "-s" => pargs.single_spaced = true,
-            _    => { 
+            _ => {
                 return Err(format!("Illegal option: {}", arg));
-           }
+            }
         };
     }
 
@@ -39,30 +39,29 @@ fn parse_aruments(args: Vec::<String>) -> Result<Arguments, String> {
 }
 
 fn print_file_contents(file: &str, count: bool, ignore_blank: bool, single_spaced: bool) {
-    
     let file_metadata = match fs::metadata(file) {
-        Ok(f)    => f,
+        Ok(f) => f,
         Err(err) => {
             println!("{}: {}", file, err);
-            return
+            return;
         }
     };
 
     if file_metadata.is_dir() {
         println!("{} is a directory", file);
-        return 
+        return;
     }
 
     let file_lines = match fs::File::open(file) {
         Ok(c) => c,
         Err(err) => {
-          println!("Error reading {}: {}", file, err);
-          return
+            println!("Error reading {}: {}", file, err);
+            return;
         }
     };
 
     let lines = io::BufReader::new(file_lines).lines();
-    
+
     let mut current_count = 1;
     let mut prev_line_blank = false;
     for line in lines {
@@ -78,20 +77,19 @@ fn print_file_contents(file: &str, count: bool, ignore_blank: bool, single_space
             }
 
             if (ignore_blank && !l.is_empty()) || count {
-               println!("{}\t {}", current_count, l);
-               current_count += 1;
+                println!("{}\t {}", current_count, l);
+                current_count += 1;
             } else {
                 println!("{}", l);
             }
         }
     }
-    
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pargs = match parse_aruments(args) {
-        Ok(args)   => args,
+        Ok(args) => args,
         Err(err) => {
             println!("{}", err);
             println!("Usage will go here.com");
@@ -100,9 +98,13 @@ fn main() {
     };
 
     for file in pargs.files {
-        print_file_contents(&file, pargs.count, pargs.count_non_blank, pargs.single_spaced);
+        print_file_contents(
+            &file,
+            pargs.count,
+            pargs.count_non_blank,
+            pargs.single_spaced,
+        );
     }
-    
+
     process::exit(0)
 }
-
--- head/src/main.rs
+++ head/src/main.rs
@@ -1,3 +1,9 @@
+struct Arguments {
+    count_by_lines: bool,
+    count_by_bytes: bool,
+    files: Vec<String>
+}
+
 fn main() {
     println!("Hello, world!");
 }
